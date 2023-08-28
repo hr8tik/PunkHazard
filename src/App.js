2321ccaae88948b5b286228d829e2117f8b4853d
unknown <hrithiksengar64@gmail.com>
2023-08-28 19:09:34 +0530

    hh

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Home from "./components/Home";
 function App() {
   return (
     <>
-      <Home></Home>
+    
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
       </Routes>
     </>
   );
